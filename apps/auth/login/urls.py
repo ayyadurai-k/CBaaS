73400515f25099b4f84a6756b7f7f9ce489a42bf
Ayyadurai <ayyadurai@victopialabs.com>
2025-08-24 18:39:49 +0530

    Added token refresh endpoint

--- apps/auth/login/urls.py
+++ apps/auth/login/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.auth.login.views import LoginView
 
 urlpatterns = [
     path("auth/login", LoginView.as_view()),
+    path("auth/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
 ]
