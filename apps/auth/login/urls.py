f38e748a133f8806bdbd15d811aa6df05f07cd90
Ayyadurai <ayyadurai@victopialabs.com>
2025-08-24 18:47:47 +0530

    `Added trailing slashes to login and token refresh URLs`

--- apps/auth/login/urls.py
+++ apps/auth/login/urls.py
@@ -4,6 +4,6 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from apps.auth.login.views import LoginView
 
 urlpatterns = [
-    path("auth/login", LoginView.as_view()),
-    path("auth/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/login/", LoginView.as_view()),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
